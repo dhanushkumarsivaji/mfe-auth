4989ef484fb8f487c17ee6d35a19ead12cfb1258
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-01-30 22:52:27 +0530

    updated auth

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/auth/latest/',
+    publicPath: 'https://mfeghworkflow.z13.web.core.windows.net/auth/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
